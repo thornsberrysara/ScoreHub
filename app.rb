d813f68bf29b392ab63573ba111aaa20d14641a9
Sara Thornsberry <thornsberrysara@gmail.com>
2021-07-15 21:08:08 -0400

    Fetched some user data

--- app.rb
+++ app.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'dotenv'
+require 'rest-client'
+require 'json'
+require 'octokit'
 
 Dotenv.load('./.env')
 
 get '/' do
   erb :index, :locals => {:client_id => ENV["CLIENT_ID"]}
+end
+
+get '/leaderboard' do
+  session_code = request.env['rack.request.query_hash']['code']
+
+  response = RestClient.post('https://github.com/login/oauth/access_token',
+    {:client_id => ENV["CLIENT_ID"],
+    :client_secret => ENV["CLIENT_SECRET"],
+    :code => session_code},
+    :accept => :json)
+  
+  access_token = JSON.parse(response)['access_token']
+  client = Octokit::Client.new(:access_token => access_token)
+  
+  user = client.user
+  avatar = user.avatar_url
+  user_login = user.login
+
+  erb :leaderboard, :locals => {:avatar_url => avatar, :login => user_login}
 end
